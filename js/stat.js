2c5bb66fe51c870d51a5a77dbc715f406b075fb8
mickbetch <serezha11shumov@mail.ru>
2018-06-05 16:29:55 +0300

    вынес во внешнюю функцию создание заголовков модального окна

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// Глобальные переменные
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
@@ -18,11 +18,13 @@ var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var TEXT_COLOR = '#000';
 var FONT_HEIGHT = '16px PT Mono';
 
+// Функция отрисовки модального окна и тени модального окна
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+// Функция получения максимального значения элемента в массиве
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
   for (var i = 1; i < arr.length; i++) {
@@ -33,14 +35,10 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-var getRandomNumber = function (min, max) {
-  Math.floor(Math.random() * (max - min) + min);
-};
-
-/*
+// Функция создания заголовков модального окна
 var titles = ['Ура вы победили!', 'Список результатов:'];
 
-var renderStatisticTitle = function (titles) {
+var renderStatisticTitle = function (ctx, titles) {
   ctx.fillStyle = TEXT_COLOR;
   ctx.textBaseline = 'hanging';
   ctx.font = FONT_HEIGHT;
@@ -48,24 +46,18 @@ var renderStatisticTitle = function (titles) {
     ctx.fillText(titles[i], CLOUD_X + GAP, CLOUD_Y + GAP + (TEXT_HEIGHT + LINE_HEIGHT) * i);
   }
 };
-*/
 
 window.renderStatistics = function (ctx, names, times) {
+
   renderCloud(ctx, CLOUD_X + 10, CLOUD_Y + 10, SHADOW_COLOR);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-  //renderStatisticTitle(titles);
-  ctx.fillStyle = TEXT_COLOR;
-  ctx.textBaseline = 'hanging';
-  ctx.font = FONT_HEIGHT;
-
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + TEXT_HEIGHT + LINE_HEIGHT);
+  renderStatisticTitle(ctx, titles);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = TEXT_COLOR;
     ctx.fillText(names[i], CLOUD_X + COLUMN_WIDTH + (COLUMN_WIDTH + COLUMN_DISTANCE) * i, PLAYERS_Y);
     ctx.fillText(Math.ceil(times[i]), CLOUD_X + COLUMN_WIDTH + (COLUMN_WIDTH + COLUMN_DISTANCE) * i, COLUMN_Y + (COLUMN_HEIGHT * times[i]) / maxTime - TEXT_HEIGHT - LINE_HEIGHT);
     if (names[i] == 'Вы') {
@@ -74,7 +66,6 @@ window.renderStatistics = function (ctx, names, times) {
       ctx.fillStyle = 'rgba(0, 0, 255, 1)';
     }
     ctx.fillRect(CLOUD_X + COLUMN_WIDTH + (COLUMN_WIDTH + COLUMN_DISTANCE) * i, COLUMN_Y, COLUMN_WIDTH, (COLUMN_HEIGHT * times[i]) / maxTime);
-
   }
 };
 
