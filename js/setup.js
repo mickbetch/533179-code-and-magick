1fa2f3dbc7c45c8de033e3ce1bb4b681bb35bca9
mickbetch <serezha11shumov@mail.ru>
2018-06-07 11:07:42 +0300

    подключил setup.js к index.html, создал массивы имен и фамилийб массив с данными о волшебниках

---
+++ js/setup.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+
+var wizards = [
+  {
+    name: '',
+    coatColor: ''
+  },
+  {
+    name: '',
+    coatColor: ''
+  },
+  {
+    name: '',
+    coatColor: ''
+  },
+  {
+    name: '',
+    coatColor: ''
+  }
+];
