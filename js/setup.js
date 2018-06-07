cf3a31e49ed13178c6c05ef3363440af37b62a6e
mickbetch <serezha11shumov@mail.ru>
2018-06-07 11:43:39 +0300

    записал функцию выбора случайного числа и добавил ее для создания случайного имени волшебника

--- js/setup.js
+++ js/setup.js
@@ -3,9 +3,9 @@
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
 
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var surNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColors = [
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SUR_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = [
   'rgb(101, 137, 164)',
   'rgb(241, 43, 107)',
   'rgb(146, 100, 161)',
@@ -13,8 +13,13 @@ var coatColors = [
   'rgb(215, 210, 55)',
   'rgb(0, 0, 0)'
 ];
-var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var getRandomWizardName = function (numbers) {
+  Math.floor(Math.random() * (numbers.length - numbers[0]) - numbers[0]);
+};
+console.log(getRandomWizardName(NAMES));
+/*
 var wizards = [
   {
     name: ,
@@ -37,3 +42,4 @@ var wizards = [
     eyesColor:
   }
 ];
+*/
