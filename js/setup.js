9158d20d3788ba037bc4f957b5088402b11223c5
mickbetch <serezha11shumov@mail.ru>
2018-06-07 11:14:46 +0300

    добавил массивы с цветами мантии и глаз

--- js/setup.js
+++ js/setup.js
@@ -5,22 +5,35 @@ userDialog.classList.remove('hidden');
 
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var wizards = [
   {
-    name: '',
-    coatColor: ''
+    name: ,
+    coatColor: ,
+    eyesColor:
   },
   {
-    name: '',
-    coatColor: ''
+    name: ,
+    coatColor: ,
+    eyesColor:
   },
   {
-    name: '',
-    coatColor: ''
+    name: ,
+    coatColor: ,
+    eyesColor:
   },
   {
-    name: '',
-    coatColor: ''
+    name: ,
+    coatColor: ,
+    eyesColor:
   }
 ];
